684e294dadb7c87d4aee7af620b6a58fc2d0ba7f
your-name <shoaibakther812@gmail.com>
2025-07-12 01:39:36 +0530

    wap to whether check given number is even or odd

--- src/day1/Program1.java
+++ src/day1/Program1.java
@@ -1,5 +1,28 @@
 package day1;
 
-public class Program1 {
+import java.util.Scanner;
 
+public class Program1 
+{
+   //Wap to check whether the given number is divisible by two or not
+   //Wap to check whether the given number is multiple of two or not 
+   //Wap to check whether the given number is divisible by two or not
+	
+	
+	public static void main(String[] args) 
+	{
+		 Scanner sc=new Scanner(System.in);
+		 
+		 System.out.println("Enter a number");
+		 int input=sc.nextInt();
+		 
+		 if(input%2==0)
+		 {
+			 System.out.println("Yes the given number "+input+" is divisible by 2");
+		 }
+		 else
+		 {
+			 System.out.println("No the given number "+input+" is not divisible by 2");
+		 }
+	}
 }
