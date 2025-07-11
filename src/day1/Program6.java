d79707f6887113be99ac6ea7f0fc616515ecf938
your-name <shoaibakther812@gmail.com>
2025-07-12 03:17:29 +0530

    Wap to eliminate last 2 digit of given number ex input 1567 than output should be 15

--- src/day1/Program6.java
+++ src/day1/Program6.java
@@ -4,10 +4,14 @@ import java.util.Scanner;
 
 public class Program6 
 {
+	//Wap to eliminate last 2 digit of given number ex input 1567 than output should be 15
+	/*Note here the logic I used is for example if I divide a number by 100 the quotient  will be before the last two digit 
+	 * ex=1984 than quotient will be 19 last two digits are eliminated likewise if you want to eliminate 3 number than
+	 * the logic would be given_number/1000
+	 */
   public static void main(String[] args) 
   {
-	//Wap to eliminate last 2 digit of given number ex input 1567 than output should be 15
-	  
+	
 	     Scanner sc=new Scanner(System.in);
 		 
 		 System.out.println("Enter a number");
