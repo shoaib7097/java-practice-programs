6b05abf32e80ebeda20dda1f660b3b8d56f4e515
your-name <shoaibakther812@gmail.com>
2025-07-12 02:43:00 +0530

    write a program to print yes if the given number is ending with 0 otherwise print No

--- src/day1/Program4.java
+++ src/day1/Program4.java
@@ -5,15 +5,16 @@ import java.util.Scanner;
 public class Program4 
 {
 	//write a program to print 'yes' if the given number is ending with 0 otherwise print 'No'
+	//Note=Here the logic is If you divide any number by 10 the remainder will be last digit of the number ex=1254%10 the rem will be 4
 	
   public static void main(String[] args)
   {
-	  Scanner sc=new Scanner(System.in);
+	     Scanner sc=new Scanner(System.in);
 		 
 		 System.out.println("Enter a number");
 		 int input=sc.nextInt();
 		 
-		 if(input%10==0)
+		 if(input%10==0)//if you get question to check the number is ending by 'n' number than condition will be given_number%10==n
 		 {
 			 System.out.println("Yes,The given number "+input+" is ending with zero");
 		 }
